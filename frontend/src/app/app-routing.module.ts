ce6032259a9d58aacdc29cdba5895de2926a15f3
jorperalto <jordon.peralto@knights.ucf.edu>
2021-12-29 13:18:29 -0500

    made create account page and changed color theme

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateAccountComponent } from './forms/create-account/create-account.component';
+//import { HomeComponent } from './home/home.component';
 
 const routes: Routes =  [
-  // { path: '',  component: HomeComponent},
+    //{ path: '',  component: HomeComponent},
+    { path: 'create-account', component: CreateAccountComponent },
 ];
 
 @NgModule({
