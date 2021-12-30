b6bf53e90f0be1bca29f1566fea3d0ab3e4aeb62
gioperalto <gyoperalto@gmail.com>
2021-12-30 18:17:57 -0500

    Added auth for login

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,7 +2,9 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 import { User } from './users/user.entity';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -16,6 +18,8 @@ import { User } from './users/user.entity';
       entities: [User],
       synchronize: true,
     }),
+    AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
